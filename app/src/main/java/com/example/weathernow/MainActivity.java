539019d72d58f13f16b332746fdcaf5dbd1c0670
Badr Ahmed <badr20126162@gmail.com>
2026-05-30 11:15:37 -0400

    First push to GitHub

---
+++ app/src/main/java/com/example/weathernow/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.weathernow;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+    }
+}
