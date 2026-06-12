e7815ef4240e00d276421c0474adfdf63c010738
Badr Ahmed <badr20126162@gmail.com>
2026-06-11 20:49:11 -0400

    Updated the App Bar

--- app/src/main/java/com/example/weathernow/MainActivity.java
+++ app/src/main/java/com/example/weathernow/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.weathernow;
 
 import android.os.Bundle;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -20,9 +24,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main, (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
         switchFragment(searchFragment);
 
         binding.bottomNavView.setOnItemSelectedListener(item -> {
